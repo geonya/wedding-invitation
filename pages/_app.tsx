eae3c0e14dea4222fcfc1f903ea4a8e5278f34b6
geonya <geony8410@gmail.com>
2022-09-13 18:19:26 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
 import '../styles/globals.css'
+import '../styles/sakura.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { MantineProvider } from '@mantine/core'
+import Sakura from '../lib/sakura'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      new Sakura('body')
+    }
+  }, [])
+  return (
+    <>
+      <Head>
+        <title>Geony ❤️ Bora Wedding Invitation</title>
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          breakpoints: {
+            xs: 380,
+            sm: 500,
+            md: 800,
+            lg: 1000,
+            xl: 1200,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  )
 }
 
 export default MyApp
